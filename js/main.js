c5eb348b109e470613e03491f9244d1eae229da0
mianivo <74909779+mianivo@users.noreply.github.com>
2024-01-12 09:46:13 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const disciplineList = document.querySelector('.list__discipline');
 const template = document.getElementById('template__form');
 const template2 = document.getElementById('template__form-2');
 const comparisonList = document.querySelector('.comparison__result .list__discipline');
-const disciplinesData = []; // Массив для хранения данных дисциплин
+const disciplinesData = [];
 
 async function fetchData() {
   try {
@@ -20,37 +20,33 @@ async function fetchData() {
 }
 
 async function showAutocomplete() {
-  var input = document.getElementById('directionInput');
-  var filter = input.value.toUpperCase();
-  var autocompleteList = document.getElementById('autocomplete-list');
+  const input = document.getElementById('directionInput');
+  const filter = input.value.toUpperCase();
+  const autocompleteList = document.getElementById('autocomplete-list');
 
-  // Если поле ввода пустое, скрыть выпадающий список
   if (input.value === '') {
       autocompleteList.style.display = 'none';
+      return
   }
 
-  // Загрузить данные из сервера
   const autocompleteData = await fetchData();
 
-  // Фильтровать элементы
   const matchingDisciplines = autocompleteData.filter(item => {
-      var txtValue = item.toUpperCase();
+      const txtValue = item.toUpperCase();
       return txtValue.indexOf(filter) > -1;
   });
 
-  // Если есть совпадения, показать выпадающий список
   if (matchingDisciplines.length > 0) {
       autocompleteList.style.display = 'block';
-      autocompleteList.innerHTML = ''; // Очистить предыдущие элементы списка
+      autocompleteList.innerHTML = ''; 
 
-      // Ограничение до 5 элементов
       const maxItems = 5;
       let displayedItems = 0;
 
       matchingDisciplines.forEach(function(item) {
         var txtValue = item;
         if (displayedItems < maxItems) {
-            var div = document.createElement('div');
+            const div = document.createElement('div');
             div.textContent = txtValue;
             div.onclick = function() {
                 input.value = txtValue;
@@ -61,13 +57,12 @@ async function showAutocomplete() {
         }
       });
   } else {
-      // Если нет совпадений, скрыть выпадающий список
       autocompleteList.style.display = 'none';
   }
 }
 
 window.addEventListener('scroll', () => {
-    var scroll = document.querySelector('.u-upward');
+    const scroll = document.querySelector('.u-upward');
     scroll.classList.toggle('active', window.scrollY > 500)
     })
 const uScrollTop = () => {
@@ -77,45 +72,34 @@ const uScrollTop = () => {
     })
 }
 
-
 addButton.addEventListener('click', function () {
-    // Получаем значения полей формы
-    const disciplineInput = document.getElementById('disciplineInput').value;
-    const countItemsInput = document.getElementById('countItemsInput').value;
-    const semesterSelect = document.getElementById('semesterSelect').value;
-    const verificationInput = document.getElementById('verificationInput').value;
+    const name = document.getElementById('disciplineInput').value;
+    const hours = parseInt(document.getElementById('countItemsInput').value, 10);
+    const semestr = parseInt(document.getElementById('semesterSelect').value, 10);
+    const is_exam = document.getElementById('verificationInput').value === 'Экзамен';
     
-
-    // const disciplineData = {
-    //     name: disciplineInput,
-    //     hours: countItemsInput,
-    //     semester: semesterSelect,
-    //     is_exam: verificationInput
-    // };
-    // disciplinesData.push(disciplineData);
-
-    // Клонируем шаблон и заполняем его данными
     const clone = template.content.cloneNode(true);
-    clone.querySelector('#outputDiscipline').value = disciplineInput;
-    clone.querySelector('#outputCountItems').value = countItemsInput;
-    clone.querySelector('#outputSemester').value = semesterSelect;
-    clone.querySelector('#outputVerificationForm').value = verificationInput;
+    clone.querySelector('#outputDiscipline').value = name;
+    clone.querySelector('#outputCountItems').value = hours;
+    clone.querySelector('#outputSemester').value = semestr;
+    clone.querySelector('#outputVerificationForm').value = is_exam ? 'Экзамен' : 'Зачет';
 
     disciplinesData.push({
-        disciplineInput,
-        countItemsInput,
-        semesterSelect,
-        verificationInput
+        name,
+        hours,
+        semestr,
+        is_exam
     });
 
-    // Добавляем клон к списку дисциплин
     disciplineList.appendChild(clone);
 
-    // Очищаем поля формы
     document.getElementById('disciplineInput').value = '';
     document.getElementById('countItemsInput').value = '';
     document.getElementById('semesterSelect').value = '';
     document.getElementById('verificationInput').value = '';
+
+    console.log(...disciplinesData)
+
 });
 disciplineList.addEventListener('click', function (event) {
     if (event.target.classList.contains('u-button-danger')) {
@@ -124,58 +108,37 @@ disciplineList.addEventListener('click', function (event) {
         const listItem = event.target.closest('.form-list');
         const index = Array.from(disciplineList.children).indexOf(listItem);
 
-        // Remove corresponding template__form-2 element
         const comparisonItem = comparisonList.children[index];
-        comparisonItem.remove();
-
-        // Remove template__form element
+        if(comparisonItem){
+            comparisonItem.remove();
+        }
         listItem.remove();
-
-        // Remove corresponding data from disciplinesData array
         disciplinesData.splice(index, 1);
     }
 });
 
-var form = document.getElementById("send__form");
-form.addEventListener("submit", async function(event) {+-+-
+const form = document.getElementById("send__form");
+form.addEventListener("submit", async function(event) {
     event.preventDefault();
-
-
-
-        var outputDiscipline = document.getElementById("outputDiscipline").value;
-    var outputCountItems = document.getElementById("outputCountItems").value;
-    var outputSemester = document.getElementById("outputSemester").value;
-    var outputVerificationForm = document.getElementById("outputVerificationForm").value;
 
     comparisonList.innerHTML = '';
 
-    // Создаем экземпляры второго шаблона для каждой добавленной дисциплины
-    disciplinesData.forEach((disciplineData) => {
-        const clone2 = template2.content.cloneNode(true);
-        clone2.querySelector('#outputYourDiscipline').value = disciplineData.disciplineInput;
-        clone2.querySelector('#outputCountItems').value = disciplineData.countItemsInput;
-        clone2.querySelector('#outputCountItemsDifferentPlan').value = ''; // Set as needed
-        clone2.querySelector('#outputYourVerificationForm-1').value = disciplineData.verificationInput;
-        clone2.querySelector('#outputNeedVerificationForm-1').value = ''; // Set as needed
-
-        // Добавляем клон к списку второго списка (результата сравнения)
-        comparisonList.appendChild(clone2);
-    });
-
-    var userData = {
-        direction_name: "Программная инженерия",
-        semestr: 1,
+    const directionInputValue = document.getElementById("directionInput").value;
+    const semesterDirectionSelectValue = document.getElementById("semesterDirectionSelect").value;
+    const userData = {
+        direction_name: directionInputValue,
+        semestr: +semesterDirectionSelectValue,
         diciplines: [
-            {
-                name: outputDiscipline,
-                hours: +outputCountItems,
-                semestr: +outputSemester,
-                is_exam: outputVerificationForm === "Экзамен"
-            }
         ]
     };
-
-    // Отправка данных на сервер
+    disciplinesData.forEach((disciplineData) => {
+        userData.diciplines.push({
+            name: disciplineData.name,
+            hours: disciplineData.hours,
+            semestr: disciplineData.semestr,
+            is_exam: disciplineData.is_exam
+        });
+    });
     const response = await fetch("http://81.177.165.51:5000/api/academic_credit_calculator", {
         method: "POST",
         headers: {
@@ -187,52 +150,42 @@ form.addEventListener("submit", async function(event) {+-+-
     if (!response.ok) {
         throw new Error("Сетевая ошибка: " + response.status);
     }
-
-    // Обработка ответа от сервера
     const responseData = await response.json();
+    console.log(responseData.compare)
     console.log(responseData)
-    if (responseData.compare && Array.isArray(responseData.compare)) {
-        const firstItem = responseData.compare[0];
 
-        if (firstItem) {
-            console.log(firstItem);
-            result(firstItem.direction_discipline, firstItem.academy_credit, firstItem.user_discipline);
-            // Here you can perform necessary actions with the first discipline
-        }
-    } else {
-        console.error("Ответ от сервера не содержит ожидаемого массива compare.");
-    }
-    
 
-});
+    responseData.compare.forEach((disciplineData) => {
+        const clone2 = template2.content.cloneNode(true);
 
-function result (data, academyСredit, userDiscipline) {
-    console.log(data)
-    if(data === null){
-        document.getElementById('outputNeedDiscipline').style.backgroundColor = 'grey';
-        document.getElementById('outputCountItemsDifferentPlan').style.backgroundColor = 'grey';
-        document.getElementById('outputNeedVerificationForm-1').value = '-';
-        document.getElementById('output__result').value = 'Да';
-        document.getElementById('output__result').style.color = '#3BA68C66';
-
-    }else{
-        document.getElementById('outputNeedDiscipline').value = data[0]
-        document.getElementById('outputCountItemsDifferentPlan').value = data[1]
-        if(data[2] === false){
-            document.getElementById('outputNeedVerificationForm-1').value = 'Зачёт'
+        if (disciplineData.direction_discipline === null) {
+            clone2.querySelector('#outputCountItemsDifferentPlan').style.backgroundColor = '#A7A7A7';
+            clone2.querySelector('#outputNeedVerificationForm-1').value = '—';
+            clone2.querySelector('#outputYourDiscipline').value = disciplineData.user_discipline.name;
+            clone2.querySelector('#outputCountItems').value = disciplineData.user_discipline.hours;
+            clone2.querySelector('#outputYourVerificationForm-1').value = disciplineData.user_discipline.is_exam ? 'Экзамен' : 'Зачет';
+            clone2.querySelector('#numberYourSemestr').textContent = disciplineData.user_discipline.semestr;
+        } else if(disciplineData.user_discipline === null) {
+            clone2.querySelector('#outputCountItems').style.backgroundColor = '#EF302B';
+            clone2.querySelector('#outputYourVerificationForm-1').value = '—';
+            clone2.querySelector('#outputNeedDiscipline').value = disciplineData.direction_discipline[0];
+            clone2.querySelector('#outputCountItemsDifferentPlan').value = disciplineData.direction_discipline[1];; // Set as needed
+            clone2.querySelector('#outputNeedVerificationForm-1').value = disciplineData.direction_discipline[2] ? 'Экзамен' : 'Зачет'; // Set as needed
+            clone2.querySelector('#numberNeedSemestr').textContent = disciplineData.direction_discipline[3];
         }else{
-            document.getElementById('outputNeedVerificationForm-1').value = 'Экзамен'
+            clone2.querySelector('#outputYourDiscipline').value = disciplineData.user_discipline.name;
+            clone2.querySelector('#outputNeedDiscipline').value = disciplineData.direction_discipline[0];
+            clone2.querySelector('#outputCountItems').value = disciplineData.user_discipline.hours;
+            clone2.querySelector('#outputCountItemsDifferentPlan').value = disciplineData.direction_discipline[1]; // Set as needed
+            clone2.querySelector('#outputYourVerificationForm-1').value = disciplineData.user_discipline.is_exam ? 'Экзамен' : 'Зачет';
+            clone2.querySelector('#outputNeedVerificationForm-1').value = disciplineData.direction_discipline[2] ? 'Экзамен' : 'Зачет'; // Set as needed
+            clone2.querySelector('#numberYourSemestr').textContent = disciplineData.user_discipline.semestr;
+            clone2.querySelector('#numberNeedSemestr').textContent = disciplineData.direction_discipline[3];
         }
-        // if(document.getElementById('outputNeedVerificationForm-1').value)
-        // document.getElementById('outputNeedVerificationForm-1').value = formExam
-        document.getElementById('output__result-credit').value = academyСredit
-        if(academyСredit <= 0 & data[2] === userDiscipline.is_exam){
-            document.getElementById('output__result').value = 'Да';
-            document.getElementById('output__result').style.color = '#3BA68C66';
-        }
-        else{
-            document.getElementById('output__result').value = 'Нет';
-            document.getElementById('output__result').style.color = 'red';
-        }
-    }
-}
+        comparisonList.appendChild(clone2);
+    });
+    const academyCredit = document.querySelector('#output__result-credit').value = responseData.academy_credit;
+    const outputResult = document.querySelector('#output__result');
+    outputResult.value = academyCredit > 0 ? 'Нет' : 'Да';
+    outputResult.style.color = academyCredit > 0 ? '#EF302B' : '#3BA68C';
+});
